96832430f33448a052d6c645a038814ccc753055
CesarRj6 <144040473+CesarRj6@users.noreply.github.com>
2026-09-18 23:50:19 +0000

    ✨ Feat: Carpeta back-end. Set up Express application with basic structure.

--- server/app.js
+++ server/app.js
@@ -8,14 +8,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 //IMPORTA EL MODULO PARA MANEJAR LOGS
 var logger = require('morgan');
-
 //IMPORTA EL MODULO PARA MANEJAR RUTAS DE LA APLICACION
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 //CREA UNA INSTANCIA DE EXPRESS (LA APLICACION)
 var app = express();
-
+ 
 //CONFIGURACION DE MOTOR DE VISTAS (HANDLEBARS)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -25,26 +23,23 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 //CONFIGURACION DE ARCHIVOS ESTATICOS (PUBLIC)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'..','public')));
 //CONFIGURACION DE RUTAS(REGISTRAMOS)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+ 
 // CAPTURA DE ERRORES 404 Y ENVIO AL MANEJADOR DE ERRORES
 app.use(function(req, res, next) {
   next(createError(404));
 });
  
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
  
-
   // RENDERIZA LA PAGINA DE ERROR CON EL ESTATUS DEL ERROR
   res.status(err.status || 500);
   res.render('error');
